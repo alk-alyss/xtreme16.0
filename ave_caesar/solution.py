04377ce73ebff1d94e12d10b92e74a405e2ca449
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2022-10-19 17:12:40 +0300

    update

--- ave_caesar/solution.py
+++ ave_caesar/solution.py
@@ -1,10 +1,13 @@
-def is_valid_string(s):
-    if len(s) == 1:
+def is_valid_string(string):
+    if len(string) == 1:
         return True
 
-    for i in range(1, len(s)):
-        s1 = s[:i]
-        s2 = s[i:]
+    for s in string:
+        if s < string[0]: return False
+
+    for i in range(1, len(string)):
+        s1 = string[:i]
+        s2 = string[i:]
 
         if s1 > s2 : continue
 
@@ -12,7 +15,6 @@ def is_valid_string(s):
         if not is_valid_string(s2): continue
 
         if s1 <= s2: return True
-
 
 
 N = int(input())
