af2f543bee8848f9f7da884c7e1f6efb0395174a
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2022-10-20 13:03:19 +0300

    not finished but it's promising

--- ave_caesar/solution.py
+++ ave_caesar/solution.py
@@ -1,85 +1,29 @@
-from functools import cache
-
-# @cache
-# def is_valid_string(string):
-#     if len(string) == 1:
-#         return True
-
-#     # if len(string) % 2 == 0 and string[:len(string) // 2] == string[len(string) // 2:]:
-#     #     return True
-
-#     for s in string:
-#         if s < string[0]: return False
-
-#     for i in range(1, len(string)):
-#         s1 = string[:i]
-#         s2 = string[i:]
-
-#         if s1 > s2: continue
-
-#         if not is_valid_string(s1): continue
-#         if not is_valid_string(s2): continue
-
-#         if s1 <= s2: return True
-
 def is_valid_string(string):
-    # substrings = []
-    # index = 0
-    # while index < len(string):
-    #     substring = string[index]
-    #     for i,s in enumerate(string[index+1:]):
-    #         if substring <= s:
-    #             substring += s
-    #         else:
-    #             substrings.append(substring)
-    #             index += i
-    #             break
+    splits = list(range(len(string)))
 
-    # next_substrings = []
-    # index = 0
-    # while index < len(substrings):
-    #     substring = substrings[index]
-    #     for i,s in enumerate(substrings[index+1:]):
-    #         if substring <= s:
-    #             substring += s
-    #         else:
-    #             next_substrings.append(substring)
-    #             index += 1
-    #             break
-
-    substrings = list(string)
-    new_substrings = []
-    prevLen = len(substrings)
-    index = 0
+    start = 0
+    end = 0
+    prevLen = len(splits)
     while True:
-        s1 = substrings[index]
-        if index >= len(substrings):
-            if prevLen == len(substrings):
-                break
+        if len(splits) == 1:
+            return True
 
-            substrings = new_substrings
-            prevLen = len(substrings)
-            index = 0
-            continue
+        if len(splits) == prevLen:
+            return False
 
-        s2 = substrings[index+1]
+        s1 = string[splits[end]:splits[end+1]]
+        end += 1
+        s2 = string[splits[end]:splits[end+1]]
 
-        if s1 <= s2:
-            s1 += s2
-            new_substrings.extend(substrings[:index])
-            new_substrings.append(s1)
-            # TODO what if index+2 does not exist
-            new_substrings.extend(substrings[index+2:])
-            index = 0
+        if s1 == [] or s2 == []:
+            start = 0
+            end = start
+
+        if s1<=s2:
+            splits.pop(end)
         else:
-            index += 1
-
-    # print(substrings)
-
-    if len(substrings) == 1:
-        return True
-
-    return False
+            start += 1
+            end = start
 
 
 N = int(input())
