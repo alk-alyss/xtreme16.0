f752346786b8c0b6422b2408f7dc5bc397293514
nikolasfil <nikolas13in@gmail.com>
2022-10-22 06:10:59 +0300

    input

---
+++ dream_team/solution.py
@@ -0,0 +1,16 @@
+def main():
+    B = int(input())
+    P = int(input())
+    point_guards = [input().split(" ") for _ in range(P)]
+    G = int(input())
+    shooting_guards = [input().split(" ") for _ in range(G)]
+    S = int(input())
+    small_forward = [input().split(" ") for _ in range(S)]
+    F = int(input())
+    power_forward = [input().split(" ") for _ in range(F)]
+    C = int(input())
+    centers = [input().split(" ") for _ in range(C)]
+
+
+if __name__ == '__main__':
+    main()
