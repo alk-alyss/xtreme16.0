a13371182752a8b8989f87ed8004726d4544112c
nikolasfil <nikolas13in@gmail.com>
2022-10-22 08:02:23 +0300

    problem dream team

--- dream_team/solution_2.py
+++ dream_team/solution_2.py
@@ -1,3 +1,8 @@
+def counter(l,step):
+    
+
+
+
 def main():
     B = int(input())
     P = int(input())
@@ -29,7 +34,6 @@ def main():
         player.sort(key=lambda x: x[1], reverse=True)
     # print(players)
 
-
     # for player in players:
     #     if len([player[i][1] for i in range(len(player))]) > len({player[i][1] for i in range(len(player))}):
     #         print('Same Value')
@@ -41,51 +45,48 @@ def main():
     selected = ['' for i in range(5)]
     budg = 0
     p, g, s, f, c = 0, 0, 0, 0, 0
-    possibilities=[]
-    while p < P:
-        while g < G:
+    while c < C:
+        while f < F:
             while s < S:
-                while f < F:
-                    while c < C:
-
-                        if players[4][c][1] + budg <= B:
-                            selected[4] = players[4][c][0]
-                            budg += players[4][c][1]
+                while g < G:
+                    while p < P:
+                        if players[0][p][1] + budg <= B:
+                            selected[0] = players[0][p][0]
+                            budg += players[0][p][1]
                             break
-                        budg -= players[4][c][1]
-                        c += 1
-                    if players[3][f][1] + budg <= B:
-                        selected[3] = players[3][f][0]
-                        budg += players[3][f][1]
+                        budg -= players[0][p][1]
+                        p += 1
+
+                    if players[1][g][1] + budg <= B:
+                        selected[1] = players[1][g][0]
+                        budg += players[1][g][1]
                         break
-                    budg += players[3][f][1]
-                    f += 1
+                    budg -= players[1][g][1]
+                    g += 1
+
                 if players[2][s][1] + budg <= B:
                     selected[2] = players[2][s][0]
                     budg += players[2][s][1]
                     break
                 budg -= players[2][s][1]
                 s += 1
-            # print(c, f, s, g, p)
-            if players[1][g][1] + budg <= B:
-                selected[1] = players[1][g][0]
-                budg += players[1][g][1]
-                # print(selected, g)
-                break
-            budg -= players[1][g][1]
 
-            g += 1
-        # print(c, f, s, g, p)
-        if players[0][p][1] + budg <= B:
-            selected[0] = players[0][p][0]
-            budg += players[0][p][1]
+            if players[3][f][1] + budg <= B:
+                selected[3] = players[3][f][0]
+                budg += players[3][f][1]
+                break
+            budg += players[3][f][1]
+            f += 1
+
+        if players[4][c][1] + budg <= B:
+            selected[4] = players[4][c][0]
+            budg += players[4][c][1]
             break
-        budg -= players[0][p][1]
-        p += 1
+        budg -= players[4][c][1]
+        c += 1
+
     print('\n'.join(selected))
 
 
 if __name__ == '__main__':
     main()
-
-
