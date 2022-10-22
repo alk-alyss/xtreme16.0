805d3034d98ece811fe64e4af600fee97b6617c7
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2022-10-22 04:16:52 +0300

    this is what I have for array

--- array/solution.py
+++ array/solution.py
@@ -1,4 +1,22 @@
+def getSolution(l, r, k, p):
+    global array
+
+
+
+
+
+
+
 n, m, p = list(map(int, input().split()))
+
+array = [0 for _ in range(n)]
 
 for _ in range(m):
     l, r, k = list(map(int, input().split()))
+    l -= 1
+    l -= 1
+
+    for i in range(l, r):
+        array[i] += 1
+
+print(array)
