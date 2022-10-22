4174b8d55292ea9b289dc8db5af66fb3f55b75fc
nikolasfil <nikolas13in@gmail.com>
2022-10-22 13:29:43 +0300

    updated

--- scheduler/solution2.py
+++ scheduler/solution2.py
@@ -3,7 +3,12 @@ def main():
     N, M = N[0], N[1]
     ls = sorted(list(map(int, input().split())), reverse=True)
 
-    
+    i = 0
+
+    while True:
+
+
+
 
 if __name__ == '__main__':
     main()
